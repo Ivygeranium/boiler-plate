f77d57c11477736f726c66e66aac5a7c6daa28fc
Ivygeranium <igeranium@naver.com>
2021-05-03 19:37:11 +0900

    create config

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+app.use(express.urlencoded({extended: true})); // application/x-www-form-urlencoded
+app.use(express.json()); //application/json
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://bolierplate:cocoyunseo01@bolierplate.nbsgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -15,6 +21,19 @@ mongoose.connect('mongodb+srv://bolierplate:cocoyunseo01@bolierplate.nbsgu.mongo
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
